9e178a58fa519bc86dc84da32415fb2d9e282eb6
kinjaer <wofus0422@gmail.com>
2023-11-30 17:48:48 +0900

    상수변수 정의, 변수 nodejs 저장, 그누보드 최신글 출력, 선택한 그누보드테마 폴더 안 css 경로 상수로 표현

--- html/kjr/index.php
+++ html/kjr/index.php
@@ -8,6 +8,8 @@ if(defined('G5_THEME_PATH')) {
     require_once(G5_THEME_PATH.'/index.php');
     return;
 }
+define("_REACT_","어렵다");
+$nodejs = "노드";
 
 if (G5_IS_MOBILE) {
     include_once(G5_MOBILE_PATH.'/index.php');
@@ -17,9 +19,12 @@ if (G5_IS_MOBILE) {
 include_once(G5_PATH.'/head.php');
 ?>
 
-<h2 class="sound_only">최신글</h2>
 
+
+<h2 class="sound_only">최신글</h2>
+<?php echo G5_CSS_URL;?>
+<?php echo latest('basic','free',5,100)?>
 <?php echo latest('pic_news_list','free',5,100); ?>
-<?php echo latest('pic_news_list','mainmenu',5,10);?>
+<?php echo latest('pic_news_list','mainmenu',5,100);?>
 <?php 
 include_once(G5_PATH.'/tail.php');
